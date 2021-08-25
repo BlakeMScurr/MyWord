1777863bc535d261ad110be04183b5b0182280cf
blakemscurr <blakemscurr@gmail.com>
2021-08-25 13:15:59 +1200

    Enable state channels wallet.

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    var exports = {}; // fixes a weird bug caused by the dynamic import as per https://stackoverflow.com/a/46887516
+    import { onMount } from "svelte";
+    import type { ChannelClient } from "@statechannels/channel-client";
+    
+    enum States {
+        waiting = 1,
+        preconnect,
+        connected,
+    }
+    let state = States.waiting;
+    let channelClient: ChannelClient | null = null;
+
+    onMount(async () => {
+        // TODO: remove dynamic import
+        // SSR of the iframe-channel-provider isn't going to work without it being refactored as it uses window everywhere
+        // We can avoid the immediate mounting by installing a sub module (channel-provider),
+        // but the logger infests the whole package (it's required by channel-provider, and at least on of channel-provider's deps)
+        // and it looks for window on install
+        let providerModule = await import('@statechannels/iframe-channel-provider');
+        await window.channelProvider.mountWalletComponent(
+            'https://xstate-wallet-v-0-3-0.statechannels.org'
+        );
+        state = States.preconnect
+    })
+    
+    async function connect() {
+        await window.channelProvider.enable();
+        state = States.connected
+    }
+</script>
+
+{#if state === States.waiting }
+    <h1>Waiting for statechannels.org wallet</h1>
+{:else if state === States.preconnect }
+    <button on:click={connect}>Connect to statechannels.org wallet</button>
+{:else if state === States.connected }
+    <h1>Connected to statechannels.org wallet</h1>
+{/if}
